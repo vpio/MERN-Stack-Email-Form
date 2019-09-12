2d1818005b3fd1f1f9d93abfd8715a2a3aa02aee
Pio Molina <vpio@comcast.net>
2019-09-11 20:14:43 -0500

    configured routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Form from './components/Form.js';
+import Thanks from './components/Thanks.js';
+import { BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Form} />
+      <Route path="/thankyou" exact component={Thanks} />
+    </BrowserRouter>
   );
 }
 
