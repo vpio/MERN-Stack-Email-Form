05a47a5816c6dfa3d9cbceb9e68b6a427ed52b1a
Pio Molina <vpio@comcast.net>
2019-09-11 21:09:01 -0500

    added validation

--- server/index.js
+++ server/index.js
@@ -8,10 +8,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/submit', (req, res) => {
-  if (req.body) {
-    console.log('Email: ', req.body.email)
-    console.log('Name: ', req.body.name)
+  if (req.body.email !== '' && req.body.name !== '') {
+    console.log('Email: ', req.body.email, 'Name: ', req.body.name)
     res.send({ success: true })
+  } else {
+    res.send({ success: false })
   }
 })
 
