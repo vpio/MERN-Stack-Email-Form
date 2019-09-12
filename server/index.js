667e17a88a159e1c978c13862a9318c8ae37887f
Pio Molina <vpio@comcast.net>
2019-09-11 21:51:41 -0500

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  email: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+mongoose.connect('mongodb+srv://vpio:ZrznvbEcR6HmIf8i@cluster0-zjfia.azure.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 const PORT = process.env.port || 3001
@@ -9,7 +19,8 @@ app.use(bodyParser.json());
 
 app.post('/submit', (req, res) => {
   if (req.body.email !== '' && req.body.name !== '') {
-    console.log('Email: ', req.body.email, 'Name: ', req.body.name)
+    console.log(`Saving ${req.body.name}'s info'`)
+    new User({ name: req.body.name, email: req.body.email}).save();
     res.send({ success: true })
   } else {
     res.send({ success: false })
